05ff3d168012edd01f62211dec46c288219de5e0
Alireza <54397250+mr18ali@users.noreply.github.com>
2021-08-08 11:54:14 +0430

    Add files via upload

---
+++ mps.py
@@ -0,0 +1,54 @@
+# python 3
+print("____________________________________________")
+
+def menu():
+    print("________________________________________")
+    print("[1] gmail") # for ex ==>all my gmails(ex==> :ali4233ir)(dont use(.,/,02,))
+    print("[2] fi website")# ex ==> gidikala,fradars,and...
+    print("[3] website")# kaliboy,and other...
+    print("[4] account")#insta,fackbook,snap,and other...if not user found typeing the company name *(facebook,instagram use id,)
+    print("________________________________________________")
+menu()
+print()
+
+option = int (input("Enter your option ==> "))
+
+if option == 1:
+
+    np = ("ynu7aerj6wpebmv8myf160w5ltb2js")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 2:
+
+    np = ("6zsdf5qb8q")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 3:
+    
+    np = ("az6k7ce2")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 4:
+
+    np = ("pkmqct4f16pa8hgbv1ao4zsxw0ckxc")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
